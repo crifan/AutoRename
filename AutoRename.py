da39b232625d5876cf44e465b7e34b2c27cc9d0a
crifanli <admin@crifan.com>
2024-01-08 11:09:09 +0800

    1. update misc for gen first para name

--- AutoRename.py
+++ AutoRename.py
@@ -1339,7 +1339,7 @@ def generateFirstParaName(selectorFirstPart):
     "setMessages_received" -> "someMessages_received"
   """
   firstParaName = ""
-  keywordMatch = re.search("(From|from|Add|add|For|With|At)(?P<keyword>[A-Z][a-z0-9_]+)$", selectorFirstPart)
+  keywordMatch = re.search("(From|from|Add|add|For|With|At|^set)(?P<keyword>[A-Z][a-z0-9_]+)$", selectorFirstPart)
   # print("keywordMatch=%s" % keywordMatch)
   if keywordMatch:
     keyword = keywordMatch.group("keyword")
